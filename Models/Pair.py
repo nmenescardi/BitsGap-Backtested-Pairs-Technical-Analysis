2b7e6b3cc12f78b30ca4dc84df32fb8288ad95e3
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-07 15:58:01 -0300

    add Pair model

---
+++ Models/Pair.py
@@ -0,0 +1,17 @@
+
+class Pair:
+    
+    categories = [] #TODO enum
+
+    def __init__(self, symbol_str, profit_str = "", category = 'MONTH'):
+        self.symbol = self.massage_text_symbol( symbol_str )
+        self.profit = self.massage_profit_number( profit_str )
+        self.category = category
+
+
+    def massage_text_symbol(self, symbol_str):
+        return symbol_str #TODO: remove / and spaces
+
+        
+    def massage_profit_number(self, profit_str):
+        return profit_str #TODO: remove % (percentage sign) and return a float
