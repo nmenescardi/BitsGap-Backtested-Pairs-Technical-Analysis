7e4ce57bb3803036aaacab92b9a8d5ebccd88313
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-08 16:15:43 -0300

    implement format data massage aux methods on Pair class

--- Models/Pair.py
+++ Models/Pair.py
@@ -4,15 +4,15 @@ class Pair:
     categories = [] #TODO enum
 
     def __init__(self, symbol_str, profit_str = "", category = 'MONTH', exchanger = 'BINANCE'):
-        self.symbol = self.massage_text_symbol( symbol_str )
-        self.profit = self.massage_profit_number( profit_str )
+        self.symbol = self.format_symbol( symbol_str )
+        self.profit = self.format_profit( profit_str )
         self.category = category
         self.exchanger = exchanger
 
 
-    def massage_text_symbol(self, symbol_str):
-        return symbol_str #TODO: remove / and spaces
+    def format_symbol(self, symbol_str):
+        return symbol_str.replace(" ", "").replace("/", "")
 
         
-    def massage_profit_number(self, profit_str):
-        return profit_str #TODO: remove % (percentage sign) and return a float
+    def format_profit(self, profit_str):
+        return float(profit_str.replace(" ", "").replace("%", ""))
