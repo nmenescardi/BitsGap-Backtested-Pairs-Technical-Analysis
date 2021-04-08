9ab4d99e6c84158a73bb472c3ffe947665bc1d3b
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-08 13:11:59 -0300

    add exchenge property into Pair class

--- Models/Pair.py
+++ Models/Pair.py
@@ -3,10 +3,11 @@ class Pair:
     
     categories = [] #TODO enum
 
-    def __init__(self, symbol_str, profit_str = "", category = 'MONTH'):
+    def __init__(self, symbol_str, profit_str = "", category = 'MONTH', exchanger = 'BINANCE'):
         self.symbol = self.massage_text_symbol( symbol_str )
         self.profit = self.massage_profit_number( profit_str )
         self.category = category
+        self.exchanger = exchanger
 
 
     def massage_text_symbol(self, symbol_str):
