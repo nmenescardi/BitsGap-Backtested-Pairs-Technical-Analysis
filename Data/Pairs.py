1794263947b7e1e286f82ee525b617c3d0af3a80
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-09 11:38:13 -0300

    cache pairs IDs

--- Data/Pairs.py
+++ Data/Pairs.py
@@ -4,6 +4,7 @@ class Pairs(AbstractDAO):
 
     def __init__(self):
         super(Pairs, self).__init__()
+        self.pair_ids = {}
 
 
     def insert(self, payload):
@@ -15,7 +16,7 @@ class Pairs(AbstractDAO):
 
 
     def __insert(self, pair): 
-        self.execute(
+        result = self.execute(
             """
                 INSERT IGNORE INTO pairs 
                     (symbol, exchanger)
@@ -26,8 +27,13 @@ class Pairs(AbstractDAO):
         )
         self.db.commit()
 
+        if result.lastrowid > 0:
+            self.pair_ids[pair.symbol] = result.lastrowid
+
 
     def get_pair_id_by_symbol(self, symbol):
+        if symbol in self.pair_ids:
+            return self.pair_ids[symbol]
 
         cursor = self.execute("SELECT pair_id FROM pairs WHERE symbol = %s;",
             (symbol,)
@@ -36,6 +42,8 @@ class Pairs(AbstractDAO):
         pair_id_result = cursor.fetchone()
 
         if pair_id_result is None:
-            return None
-
-        return pair_id_result[0]
+            return None #TODO: raise custom exception
+        else:
+            pair_id = pair_id_result[0]
+            self.pair_ids[symbol] = pair_id
+            return pair_id
